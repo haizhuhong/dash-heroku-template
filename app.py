316ad4c6a1adeda12e91368c95c06285f2c8c8c6
Haizhu Hong <hong8890@163.com>
2020-11-20 05:54:24 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,9 @@
 import numpy as np
 import pandas as pd
-import plotly.graph_objects as go
 import plotly.express as px
-import plotly.graph_objects as go
 import plotly.figure_factory as ff
 
 import dash
-from jupyter_dash import JupyterDash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
